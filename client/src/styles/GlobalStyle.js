8df4ed4835a4606191a589f8195644207d90130a
parkjeyoung <dkfmwpsxm@naver.com>
2022-03-02 20:32:05 +0900

    feat : 전적 페이지 추가 및 themeProvider 작성

---
+++ client/src/styles/GlobalStyle.js
@@ -0,0 +1,29 @@
+import {createGlobalStyle} from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+*, *::before, *::after {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+}
+
+* {
+    font-family: 'Roboto', sans-serif;
+    margin: 0;
+    padding: 0;
+}
+
+body {
+        font-family: "Helvetica", "Arial", sans-serif;
+    line-height: 1.5;
+    box-sizing: border-box;
+    padding: 0px;
+}
+
+a {
+    text-decoration: none;
+}
+
+`;
+
+export default GlobalStyle;
