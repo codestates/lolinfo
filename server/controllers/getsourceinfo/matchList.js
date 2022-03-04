fa87715f89a3a30657ca1a741c948640805c96e1
happy5happy5 <zombil8731@gmail.com>
2022-03-05 01:12:07 +0900

    refactor : 클래스 형태 모듈 사용

--- server/controllers/getsourceinfo/matchList.js
+++
@@ -1,8 +0,0 @@
-require("dotenv").config();
-const axios = require("axios");
-module.exports = async (puuid, int1 = 0, int2 = 20) => {
-    let data = await axios.get(`https://asia.api.riotgames.com/lol/match/v5/matches/by-puuid/${puuid}/ids?start=${int1}&count=${int2}`, {
-        headers: { "X-Riot-Token": process.env.API || "RGAPI-5b4398c7-9bcb-479d-931c-22d4a24cc71c" },
-    });
-    return data.data;
-};
--- server/controllers/getsourceinfo/puuid.js
+++
@@ -1,9 +0,0 @@
-require("dotenv").config();
-const axios = require("axios");
-module.exports = async (nickname) => {
-    let encodedName = encodeURI(nickname);
-    let data = await axios.get(`https://kr.api.riotgames.com/lol/summoner/v4/summoners/by-name/${encodedName}`, {
-        headers: { "X-Riot-Token": process.env.API || "RGAPI-5b4398c7-9bcb-479d-931c-22d4a24cc71c" },
-    });
-    return data;
-};
