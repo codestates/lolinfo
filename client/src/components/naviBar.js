50f81f6744027802a133942fd2d3566e699e3a71
happy5happy5 <zombil8731@gmail.com>
2022-03-04 11:33:46 +0900

    feat: 라우터 기능

--- client/src/components/naviBar.js
+++ client/src/components/naviBar.js
@@ -1,16 +1,34 @@
 import React from 'react';
 import styled from 'styled-components';
-
+import { useNavigate} from 'react-router-dom'
 
 
 
 function NaviBar({sticky,naviMenu}){
+  let navigate = useNavigate();
+  function onClickMenu(order){
+    console.log(order)
+    if(order===0){//메인
+      // avigate
+    }
+    else if(order===1){//게시판
+      
+    }
+    else if(order===2){//전적
+      navigate('/record')
+    }
+    else if(order===3){//랭킹
 
+    }
+    else if(order===4){//오픈채팅
+
+    }
+  }
     return(
         <div >
             <NaviContainer className={sticky ? "naviBar-sticky" : null}>
         {
-            menuNameList.map((x,i)=><Menu key={i} order={i} onClick={()=>naviMenu(i)}>{x}</Menu>)
+            menuNameList.map((x,i)=><Menu key={i} order={i} onClick={()=>{naviMenu(i);onClickMenu(i)}}>{x}</Menu>)
         }
             </NaviContainer>
         </div>
