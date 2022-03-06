b94f41545cf96fc6b9e6604726bec91a9e18afa1
HyesungYoon <yhs9421877@gmail.com>
2022-03-06 16:49:43 +0900

    feat: 시퀄라이즈 마이그레이션 및 모델 작성

---
+++ server/models/reply.js
@@ -0,0 +1,36 @@
+"use strict";
+const { Model } = require("sequelize");
+module.exports = (sequelize, DataTypes) => {
+  class Reply extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+      Reply.hasMany(models.Reply);
+      Reply.belongsTo(models.User);
+      Reply.belongsTo(models.Board);
+    }
+  }
+  Reply.init(
+    {
+      id: {
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+        primaryKey: true,
+        comment: "고유번호 UUID",
+      },
+      body: DataTypes.STRING,
+      class: DataTypes.TINYINT,
+    },
+    {
+      sequelize,
+      modelName: "Reply",
+      timestamps: true,
+      paranoid: true,
+    },
+  );
+  return Reply;
+};
