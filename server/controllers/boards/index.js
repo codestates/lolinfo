0dde74682585929daa8e4f0f96815f8632dda5eb
HyesungYoon <yhs9421877@gmail.com>
2022-03-07 17:51:17 +0900

    fix: 모델 요구사항 수정, salt

--- server/controllers/boards/index.js
+++ server/controllers/boards/index.js
@@ -1,13 +1,34 @@
-const { board } = require('../../models')
+const { Board, User } = require("../../models");
 
 module.exports = {
-  newPost: (req, res) => {
-    res.send('ok');
+  newPost: async (req, res) => {
+    //validation
+    const { title, body, userId } = req.body;
+    if (!title || !body || !userId) {
+      res.status(400).send({
+        message: "Bad Request",
+      });
+      return;
+    }
+
+    // userId validation
+    // const user = await User.findByPk(userId);
+    // if (user === null) {
+    //   res.status(400).send('Bad Request: user obscure');
+    //   return;
+    // }
+
+    const post = await Board.create({ title, body, userId });
+    res.send({
+      message: `Post Created: ${post.id}`,
+      boardId: post.id,
+    });
   },
   listPost: (req, res) => {
+    // defaults: 0 to 25 post
+    const { limit = 25, offset = 0 } = req.body;
 
+    
   },
-  deletePost: (req, res) => {
-
-  }
-}
+  deletePost: (req, res) => {},
+};
