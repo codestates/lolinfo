59edab447779961218c0d68b28c5092a9aed3006
parkjeyoung <dkfmwpsxm@naver.com>
2022-03-08 01:25:23 +0900

    feat: record page API ver 1.3

--- client/src/pages/RecordPage.js
+++ client/src/pages/RecordPage.js
@@ -4,7 +4,6 @@ import UserProfile from "./pageComponents/UserProfile";
 import RecentChart from "./pageComponents/RecentChart";
 import RecentGameLog from "./pageComponents/RecentGameLog";
 import { useSelector, useDispatch } from "react-redux";
-import { getProfile } from "../store/GameProfile";
 import { getRecord } from "../store/GameRecord";
 
 const Content = styled.div`
@@ -54,14 +53,10 @@ const LogWrapper = styled.div`
 `;
 
 function RecordPage() {
-  // const { data: profile } = useSelector((state) => state.gameProfile);
   const { data: record } = useSelector((state) => state.gameRecord);
-  console.log("record=", record);
+  console.log("record", record);
   const dispatch = useDispatch();
 
-  // useEffect(() => {
-  //   console.log("profile:::", profile);
-  // }, [profile]);
   // useEffect(() => {
   //   console.log("record:::", record);
   // }, [record]);
@@ -80,11 +75,7 @@ function RecordPage() {
   return (
     <div>
       <Content>
-<<<<<<< HEAD
-        <UserProfile info={profile.data} />
-=======
-        {/* <UserProfile /> */}
->>>>>>> c8110497bb007752869a01c8020b0ecd4fd6f404
+        <UserProfile info={record.data[0][0]} />
         <BoxWrapper name="BoxWrapper">
           <RecentChart className="RecentChart" />
           <div>
