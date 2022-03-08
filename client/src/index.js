c52884f540cf7cab2df868da73e293c23a0178df
parkjeyoung <dkfmwpsxm@naver.com>
2022-03-08 15:29:21 +0900

    feat: record page API 1.8

--- client/src/index.js
+++ client/src/index.js
@@ -10,7 +10,7 @@ import { composeWithDevTools } from "redux-devtools-extension";
 import logger from "redux-logger";
 import rootReducer from "./store";
 
-const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(ReduxThunk, logger)));
+const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(ReduxThunk)));
 
 ReactDOM.render(
   <React.StrictMode>
