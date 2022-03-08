cffb60d0a0b0b0e9c0c8bebcd5a1705a97eaf118
happy5happy5 <zombil8731@gmail.com>
2022-03-08 09:19:32 +0900

    feat record page for loop

--- client/src/pages/RecordPage.js
+++ client/src/pages/RecordPage.js
@@ -52,11 +52,9 @@ const LogWrapper = styled.div`
 
   background-color: ${(props) => props.theme.recordBgColor};
 `;
-
 function RecordPage() {
   // const { data: profile } = useSelector((state) => state.gameProfile);
   const { data: record } = useSelector((state) => state.gameRecord);
-  console.log("record=", record);
   const dispatch = useDispatch();
 
   // useEffect(() => {
@@ -66,6 +64,9 @@ function RecordPage() {
   //   console.log("record:::", record);
   // }, [record]);
 
+  // useEffect(() => {
+  //   console.log("temp=", temp);
+  // }, [temp]);
   useEffect(() => {
     // const profileUrl = "http://localhost:80/games/player?nickname=";
     // dispatch(getProfile("get", profileUrl, "고양이"));
@@ -76,7 +77,50 @@ function RecordPage() {
 
   if (record.loading) return <div>로딩중...</div>;
   if (!record.data) return <div>data null!...</div>;
+  let temp = {};
+  let user = "고양이";
+  for (let i = 1; i < record.data.length; i++) {
+    if (!temp.gameDuration) temp.gameDuration = [];
+    temp.gameDuration.push(record.data[i].info.gameDuration);
+    if (!temp.gameMode) temp.gameMode = [];
+    temp.gameMode.push(record.data[i].info.gameMode);
+    //여기서 서머너이름에 따른 경기를 찾는다
+    let aux = record.data[i].info.participants;
+    for (let j = 0; j < aux.length; j++) {
+      if (aux[j].summonerName === user) {
+        console.log(aux[j]);
+        if (!temp.assists) temp.assists = [];
+        temp.assists.push(aux[j].assists);
+        if (!temp.championName) temp.championName = [];
+        temp.championName.push(aux[j].championName);
+        if (!temp.championId) temp.championId = [];
+        temp.championId.push(aux[j].championId);
+        if (!temp.championName) temp.championName = [];
+        temp.championName.push(aux[j].championName);
+        if (!temp.deaths) temp.deaths = [];
+        temp.deaths.push(aux[j].deaths);
+        if (!temp.goldEarned) temp.goldEarned = [];
+        temp.goldEarned.push(aux[j].goldEarned);
+        if (!temp.item) temp.item = [];
+        temp.item.push([aux[j].item0, aux[j].item1, aux[j].item2, aux[j].item3, aux[j].item4, aux[j].item5, aux[j].item6]);
+        if (!temp.kills) temp.kills = [];
+        temp.kills.push(aux[j].kills);
+        if (!temp.profileIcon) temp.profileIcon = [];
+        temp.profileIcon.push(aux[j].profileIcon);
+        if (!temp.teamId) temp.teamId = [];
+        temp.teamId.push(aux[j].teamId);
+        if (!temp.win) temp.win = [];
+        temp.win.push(aux[j].win);
+        if (!temp.summonerspell) temp.summonerspell = [];
+        temp.win.push([aux[j].summoner1Id, aux[j].summoner2Id]);
+      }
+    }
 
+    //   summonerName;
+    //   if (!temp.gameDuration) temp.gameDuration = [];
+    //   temp.gameDuration.push(record.data[i].info.gameDuration);
+  }
+  console.log(temp);
   return (
     <div>
       <Content>
