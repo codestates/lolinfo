8d383453572eb1c7283b8b4999fe46ff1ee53bfd
applehole <skyheo123@naver.com>
2022-03-09 14:44:23 +0900

    fix: 로그인 오류 수정

--- client/src/components/signupPage.js
+++ client/src/components/signupPage.js
@@ -3,9 +3,9 @@ import styled from "styled-components";
 import axios from "axios";
 import { validEmail, validPassword, isMatchPassword } from "../modules/validation"
 
-
 axios.defaults.withCredentials = true;
-function SignupPage({ setLoginModal, setUserInfo, userInfo }) {
+
+function SignupPage({ setLoginModal, setUserInfo, userInfo, setemailState }) {
 
   const EmailInputFunction = (e) => {
     setUserInfo(Object.assign(userInfo, { "email": e.target.value }))
@@ -27,7 +27,7 @@ function SignupPage({ setLoginModal, setUserInfo, userInfo }) {
     // console.log(userInfo); //이해를 돕기위해 남겨놓겠습니다.
     const { name, email, password, passwordC } = userInfo
     if (!validEmail(email)) {
-      return alert("이메일 형식이 아닙니다.")
+      return setemailState("change")// 이메일 형식이 아니라면 모달창
     }
     if (!validPassword(password)) {
       return alert("비밀번호 조건: 8~16자 영문 대 소문자, 숫자, 특수문자를 사용해야합니다.")
@@ -35,7 +35,7 @@ function SignupPage({ setLoginModal, setUserInfo, userInfo }) {
     if (!isMatchPassword(password, passwordC)) {
       return alert("비밀번호가 일치하지 않습니다.")
     }
-    const LoginReturnValue = await axios.post(`${process.env.REACT_APP_API_URL}/users/register`, { name: name, email: email, password: password })
+    const LoginReturnValue = await axios.post(process.env.REACT_APP_API_URL + "/users/register", { name: name, email: email, password: password })
     if (LoginReturnValue.status === 201) {
       alert("정상적으로 회원가입이 되었습니다.")
       setLoginModal("login")
--- server/controllers/users/logout.js
+++ server/controllers/users/logout.js
@@ -1,5 +1,9 @@
 module.exports = {
   post: (req, res) => {
+    if (req.body.email === undefined) {
+      return res.status(400).send('Not Enough Data');
+    }
+
     res.status(205).send('Logged out successfully');
   },
 };
