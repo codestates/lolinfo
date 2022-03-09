1f87b7cfd5841e9d5fb5cc7079583ff4c97bc8ab
applehole <skyheo123@naver.com>
2022-03-09 16:41:01 +0900

    feat: 모달창 ver 1.1

--- client/src/App.js
+++ client/src/App.js
@@ -28,6 +28,10 @@ function App() {
   const [loginState, setLoginState] = useState("");
   const [loginFailState, setloginFailState] = useState("");
   const [emailState, setemailState] = useState("");
+  const [passwordState, setPasswordState] = useState("");
+  const [passwordCheckState, setPasswordCheckState] = useState("");
+  const [registerState, setRegisterState] = useState("");
+  const [replaceState, setReplaceState] = useState("");
   const [userInfo, setUserInfo] = useState({
     email: "",
     name: "",
@@ -56,18 +60,23 @@ function App() {
           {loginModal ? (
             <Modal setLoginModal={setLoginModal} visible={true}>
               {loginModal === "login" ? <LoginPage setLoginModal={setLoginModal} userInfo={userInfo} setUserInfo={setUserInfo} setLoginState={setLoginState} setloginFailState={setloginFailState} />
-                : <SignupPage setLoginModal={setLoginModal} setUserInfo={setUserInfo} userInfo={userInfo} setemailState={setemailState} />}
+                : <SignupPage setLoginModal={setLoginModal} setUserInfo={setUserInfo} userInfo={userInfo} setemailState={setemailState} setPasswordState={setPasswordState} setPasswordCheckState={setPasswordCheckState} setRegisterState={setRegisterState} />}
             </Modal>
           ) : null}
           {loginState ? <AlertModal setLoginState={setLoginState} visible={true} children="로그인이 완료되었습니다."></AlertModal> : <div></div>}
-          {loginFailState ? <AlertModal setloginFailState={setloginFailState} visible={true} children="로그인에 실패했습니다."></AlertModal> : <div></div>}
-          {emailState ? <AlertModal setemailState={setemailState} visible={true} children="이메일 형식이 아닙니다."></AlertModal> : <div></div>}
+          {loginFailState ? <AlertModal setLoginState={setloginFailState} visible={true} children="로그인에 실패했습니다."></AlertModal> : <div></div>}
+          {emailState ? <AlertModal setLoginState={setemailState} visible={true} children="이메일 형식이 아닙니다."></AlertModal> : <div></div>}
+          {passwordState ? <AlertModal setLoginState={setPasswordState} visible={true} children="비밀번호 조건: 8~16자 영문 대 소문자, 숫자, 특수문자를 사용해야합니다."></AlertModal> : <div></div>}
+          {passwordCheckState ? <AlertModal setLoginState={setPasswordCheckState} visible={true} children="비밀번호가 일치하지 않습니다."></AlertModal> : <div></div>}
+          {registerState ? <AlertModal setLoginState={setRegisterState} visible={true} children="회원가입에 성공했습니다!"></AlertModal> : <div></div>}
+          {replaceState ? <AlertModal setLoginState={setReplaceState} visible={true} children="정상적으로 교체되었습니다!"></AlertModal> : <div></div>}
+
           <Routes>
             <Route path="/" element={<Home setHistory={setHistory} />} />
             <Route path="/board" element={<Board setHistory={setHistory} />} />
             <Route path="/record" element={<RecordPage setHistory={setHistory} />} />
-            <Route path="/mypage/edit" element={<Edit setHistory={setHistory} />} />
-            <Route path="/mypage/changePassword" element={<ChangePassword setHistory={setHistory} />} />
+            <Route path="/mypage/edit" element={<Edit setHistory={setHistory} setReplaceState={setReplaceState} />} />
+            <Route path="/mypage/changePassword" element={<ChangePassword setHistory={setHistory} setPasswordState={setPasswordState} setPasswordCheckState={setPasswordCheckState} setReplaceState={setReplaceState} />} />
             <Route path="/mypage/deleteAccount" element={<DeleteAccount setHistory={setHistory} />} />
             <Route path="/chat" element={<ChattingRoom setHistory={setHistory} />} />
           </Routes>
